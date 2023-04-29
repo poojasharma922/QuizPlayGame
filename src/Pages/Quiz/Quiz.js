433c328c1e4de8c6c3541252cb7b426194a143d8
Pooja <pooja123nisha@gmail.com>
2023-04-30 04:10:52 +0530

    Demo Commit

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,3 +1,51 @@
+// import { CircularProgress } from "@material-ui/core";
+// import { useEffect, useState } from "react";
+// import Question from "../../components/Question/Question";
+// import "./Quiz.css";
+
+// const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
+//   const [options, setOptions] = useState();
+//   const [currQues, setCurrQues] = useState(0);
+
+//   useEffect(() => {
+//     setOptions(questions && handleShuffle([
+//           questions[currQues]?.correct_answer,
+//           ...questions[currQues]?.incorrect_answers,
+//         ])
+//     );
+//   }, [currQues, questions]);
+
+//   console.log(questions);
+
+//   const handleShuffle = (options) => {
+//     return options.sort(() => Math.random() - 0.5);
+//   };
+
+//   return (
+//     <div className="quiz">
+//       <span className="subtitle">Welcome, {name}</span>
+
+//       {questions ? (
+//         <>
+//           <div className="quizInfo">
+//             <span>{questions[currQues].category}</span>
+//             <span>
+//               {/* {questions[currQues].difficulty} */}
+//               Score : {score}
+//             </span>
+//           </div>
+//           <Question currQues={currQues} setCurrQues={setCurrQues} questions={questions} options={options} correct={questions[currQues]?.correct_answer} score={score} setScore={setScore} setQuestions={setQuestions} />
+//         </>
+//       ) : (
+//         <CircularProgress style={{ margin: 100 }} color="inherit" size={150} thickness={1} />
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Quiz;
+
+
 import { CircularProgress } from "@material-ui/core";
 import { useEffect, useState } from "react";
 import Question from "../../components/Question/Question";
@@ -8,12 +56,18 @@ const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
   const [currQues, setCurrQues] = useState(0);
 
   useEffect(() => {
-    setOptions(questions && handleShuffle([
+    setOptions(
+      questions &&
+        handleShuffle([
           questions[currQues]?.correct_answer,
           ...questions[currQues]?.incorrect_answers,
         ])
     );
   }, [currQues, questions]);
+
+  useEffect(() => {
+    setScore(0); // Reset the score when the component is mounted
+  }, []); 
 
   console.log(questions);
 
@@ -29,15 +83,26 @@ const Quiz = ({ name, questions, score, setScore, setQuestions }) => {
         <>
           <div className="quizInfo">
             <span>{questions[currQues].category}</span>
-            <span>
-              {/* {questions[currQues].difficulty} */}
-              Score : {score}
-            </span>
+            <span>Score: {score}</span>
           </div>
-          <Question currQues={currQues} setCurrQues={setCurrQues} questions={questions} options={options} correct={questions[currQues]?.correct_answer} score={score} setScore={setScore} setQuestions={setQuestions} />
+          <Question
+            currQues={currQues}
+            setCurrQues={setCurrQues}
+            questions={questions}
+            options={options}
+            correct={questions[currQues]?.correct_answer}
+            score={score}
+            setScore={setScore}
+            setQuestions={setQuestions}
+          />
         </>
       ) : (
-        <CircularProgress style={{ margin: 100 }} color="inherit" size={150} thickness={1} />
+        <CircularProgress
+          style={{ margin: 100 }}
+          color="inherit"
+          size={150}
+          thickness={1}
+        />
       )}
     </div>
   );
